ccc40c986267f74d82ccb49fabdcda379056c55f
Alex Bills <email": null>
2016-08-04 11:06:16 -0400

    rebased

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,12 +15,13 @@ class GamesController < ApplicationController
       @number = current_game.id
       @message = "Second player has not arrived."
       render 'hold'
-    elsif game_over?
-      render '/over'
+    # elsif game_over?
+    #   render '/over'
     else
       if current_game.tiles.where(player_id: @current_user.id).empty?
         current_game.create_tiles(@current_user.id)
         current_game.create_opponent_tiles(opponent.id)
+        set_up_ships(@current_game)
       end
     end
       @opponent_board = current_game.tiles.where(player_id: opponent.id)
@@ -63,14 +64,14 @@ class GamesController < ApplicationController
     row = params[:row]
     col = params[:column]
     coord = row + ', ' + col
-    tile = Tile.find_by(coordinates: coord, game_id: params[:id], player_id: @current_user.id)
+    tile = Tile.find_by(coordinates: coord, game_id: params[:id], player_id: opponent.id)
     if tile
       tile.hit = true
       tile.save
-      if ship_hit(tile)
-        redirect_to "/hit?ship_id=#{tile.ship_id}"
+      if tile.ship_id
+        redirect_to "/games/#{@current_game.id}/hit"
       else
-        redirect_to '/show'
+        redirect_to "/games/#{@current_game.id}"
       end
     else
       render 'show'
@@ -79,11 +80,6 @@ class GamesController < ApplicationController
   end
 
 private
-
-  # def fire_params
-  #   binding.pry
-  #   params.require(:fire).permit(:row, :column)
-  # end
 
   def logged_in?
     session[:user_id]
@@ -99,14 +95,6 @@ private
     @current_game ||= Game.find(params[:id])
   end
 
-  # def which_player
-  #   game = Game.find(params[:id])
-  #   if @current_user == game.player_1
-  #     player_1 = @current_user
-  #   elsif @current_user == game.player_2
-  #     player_2 = @current_user
-  #   end
-  # end
 
   def opponent
     if @current_user == current_game.player_1
@@ -116,68 +104,54 @@ private
     end
   end
 
-  def ship_hit(tile)
-    opp_tile = @opponent.tiles.find_by(coordinates: tile.coordinates)
-    if opp_tile.ship_id == nil
-      false
-    else
-      Ship.find(opp_tile.ship_id).type
-    end
-  end
 
   def game_over?
-    p1_ships = current_game.ships.where(game_id: current_game.id)
-    if !p1_ships.empty?
+    p1_ships = current_game.ships.where(player_id: @current_user.id)
       p1_ships.each do |ship|
-          p1_in_use = ship.tiles.take_while { |tile| tile.hit == true }
+          p1_in_use = ship.tiles.reject { |tile| tile.hit == false }
       end
-      p2_ships = current_game.ships.where(player_id: @opponent)
+      p2_ships = current_game.ships.where(player_id: opponent.id)
       p2_ships.each do |ship|
-          p2_in_use = ship.tiles.take_while { |tile| tile.hit == true }
+          p2_in_use = ship.tiles.reject { |tile| tile.hit == false }
       end
       if p1_in_use.empty? || p2_in_use.empty?
         true
       else
         false
       end
-    else
-      false
-    end
   end
 
 
   def set_up_ships(game)
-    aircraft = Ship.new(name: "Aircraft Carrier", length: 5, game: game)
-    battle = Ship.new(name: "Battleship", length: 4, game: game)
-    sub = Ship.new(name: "Submarine", length: 1, game: game)
-    destroyer = Ship.new(name: "Destroyer", length: 2, game: game)
-    tom = Ship.new(name: "Cruiser", length: 3, game: game)
-    aircraft2 = Ship.new(name: "Aircraft Carrier", length: 5, game: game)
-    battle2 = Ship.new(name: "Battleship", length: 4, game: game)
-    sub2 = Ship.new(name: "Submarine", length: 1, game: game)
-    destroyer2 = Ship.new(name: "Destroyer", length: 2, game: game)
-    tom2 = Ship.new(name: "Cruiser", length: 3, game: game)
+    aircraft = Ship.create(name: "Aircraft Carrier", length: 5, game: game)
+    # battle = Ship.create(name: "Battleship", length: 4, game: game)
+    # sub = Ship.create(name: "Submarine", length: 1, game: game)
+    # destroyer = Ship.create(name: "Destroyer", length: 2, game: game)
+    # tom = Ship.create(name: "Cruiser", length: 3, game: game)
+    # aircraft2 = Ship.create(name: "Aircraft Carrier", length: 5, game: game)
+    # battle2 = Ship.create(name: "Battleship", length: 4, game: game)
+    # sub2 = Ship.create(name: "Submarine", length: 1, game: game)
+    # destroyer2 = Ship.create(name: "Destroyer", length: 2, game: game)
+    # tom2 = Ship.create(name: "Cruiser", length: 3, game: game)
 
-    place_ship_at("c, 7", "vertical", tom, game, game.player_1)
-    place_ship_at("a, 1", "horizontal", sub, game, game.player_1)
+    # place_ship_at("c, 7", "vertical", tom, game, game.player_1)
+    # place_ship_at("a, 1", "horizontal", sub, game, game.player_1)
     place_ship_at("i, 2", "horizontal", aircraft, game, game.player_1)
-    place_ship_at("f, 5", "vertical", battle, game, game.player_1)
-    place_ship_at("b, 10", "vertical", destroyer, game, game.player_1)
+    # place_ship_at("f, 5", "vertical", battle, game, game.player_1)
+    # place_ship_at("b, 10", "vertical", destroyer, game, game.player_1)
 
-    place_ship_at("e, 4", "vertical", tom2, game, game.player_2)
-    place_ship_at("j, 9", "horizontal", sub2, game, game.player_2)
-    place_ship_at("d, 1", "horizontal", aircraft2, game, game.player_2)
-    place_ship_at("g, 9", "vertical", battle2, game, game.player_2)
-    place_ship_at("i, 3", "vertical", destroyer2, game, game.player_2)
-
-    p1_tiles = game.player_1.tiles
+    # place_ship_at("e, 4", "vertical", tom2, game, game.player_2)
+    # place_ship_at("j, 9", "horizontal", sub2, game, game.player_2)
+    # place_ship_at("d, 1", "horizontal", aircraft2, game, game.player_2)
+    # place_ship_at("g, 9", "vertical", battle2, game, game.player_2)
+    # place_ship_at("i, 3", "vertical", destroyer2, game, game.player_2)
   end
 
 
   def place_ship_at(coordinate, orientation, ship, game, player)
     valid_tiles = []
     if valid_coordinate?(coordinate)
-      potential_tile = game.player_1.tiles.find_by(coordinate: coordinate)
+      potential_tile = game.tiles.find_by(coordinates: coordinate, player_id: player.id)
       if !potential_tile.ship
         valid_tiles << potential_tile
       else
@@ -189,10 +163,10 @@ private
       ship_tail.times do
         if orientation == "horizontal"
           number += 1
-          if !valid_coordinate("#{letter.chr}, #{number}")
+          if !valid_coordinate?("#{letter.chr}, #{number}")
             return nil
           else
-            potential_tile = game.player.tiles.find_by(coordinate: "#{letter.chr}, #{number}")
+            potential_tile = game.tiles.find_by(coordinates: "#{letter.chr}, #{number}", player_id: player.id)
             if !potential_tile.ship
               valid_tiles << potential_tile
             else
@@ -201,11 +175,11 @@ private
           end
         elsif orientation == "vertical"
           letter += 1
-          if !valid_coordinate("#{letter.chr}, #{number}")
+          if !valid_coordinate?("#{letter.chr}, #{number}")
             return nil
           else
-            potential_tile = game.player.tiles.find_by(coordinate: "#{letter.chr}, #{number}")
-            if !potential_tile.ship
+            potential_tile = game.tiles.find_by(coordinates: "#{letter.chr}, #{number}", player_id: player.id)
+            if potential_tile.ship.nil?
               valid_tiles << potential_tile
             else
               return nil
@@ -223,7 +197,7 @@ private
 
   def valid_coordinate?(coordinate)
     letter = coordinate[0]
-    number = coordinate[-1]
+    number = coordinate[-1].to_i
     if letter.ord > "j".ord
       return false
     elsif letter.ord < "a".ord
