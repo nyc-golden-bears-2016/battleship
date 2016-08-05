6c76ddedd6bbeb9093da364434911185c81cd8a7
ericatalyst <erica825978@gmail.com>
2016-08-05 12:06:14 -0400

    Spacing

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
-
-  setInterval(ajaxCall, 1000) ;
-
+  setInterval(ajaxCall, 1000);
   function ajaxCall() {
     $.ajax({
       url: "/games/" + $('#game-id').val() + '/what_turn'
     })
     .done(function(response) {
-      console.log('MONEY')
       if(response === true){
         $('.fire').show();
       }
